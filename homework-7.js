c5da563e4cc041cb6632878b37e5d3ed46ed1075
rasul <dzohartulparovic@gmail.com>
2026-08-25 20:31:22 +0300

    I passed the test and solved the tasks

---
+++ homework-7.js
@@ -0,0 +1,36 @@
+function showWeather(city, temperature) {
+  console.log(`Сейчас в городе ${city} температура ${temperature} градусов по Цельсию.`)
+}
+showWeather("Черкесск",30)
+
+const LIGHT_SPEED =  299792458
+
+const compareSpeed = (speed) => {
+  if (speed > LIGHT_SPEED) {
+    console.log("Сверхсветовая скорость")
+  } else if (speed < LIGHT_SPEED){
+    console.log("Субсветовая скорость")
+  } else if (speed === LIGHT_SPEED){
+    console.log("Скорость света");
+  }
+};
+
+  compareSpeed(2997924589);
+  compareSpeed(29979245);
+  compareSpeed(299792458);
+  
+
+const product = "Гардош Хычын";
+const price = 100;
+
+const buyProduct = (budget) => {
+  if (budget >= price) {
+    console.log(`${product} приобретен Спасибо за покупку!`)
+  } else {
+    const difference = price - budget;
+    console.log(`Вам не хватает ${difference}, пополните баланс`)
+  }
+};
+
+  buyProduct(100);
+  buyProduct(90);
